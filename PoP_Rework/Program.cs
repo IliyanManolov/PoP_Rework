9c8c9c128c943ca4374dee1f47b68461478433d0
IliyanManolov <killerood1@gmail.com>
2023-03-17 16:22:49 +0200

    Add project files.

---
+++ PoP_Rework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP_Rework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ PoP_Rework/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP_Rework
+{
+    public class Student:Person
+    {
+
+    }
+}
