04f887ceea64bab588837c4f246d242acf4ca75a
IliyanManolov <killerood1@gmail.com>
2023-03-17 17:33:31 +0200

    Basic functionalities, need to figure out how to do the address

--- PoP_Rework/Program.cs
+++ PoP_Rework/Program.cs
@@ -10,7 +10,24 @@ namespace PoP_Rework
     {
         static void Main(string[] args)
         {
+            Address address = new Address();
+            address.ExactAddress = "testExactAddress";
+            address.Street = "testStreet";
+            address.City = "testCity";
+            address.Country = "testCountry";
 
+            Student student = new Student();
+            student.FirstName = "TestFirstName";
+            student.LastName = "TestLastName";
+            student.FullAddress = student.SetFullAddress(address);
+            //student.FullAddress = "TEST ADDRESS";
+            student.StudentNumber = "TEST NUMBER";
+            int[] test = new int[] { 1, 2, 3 };
+            student.Scores = test;
+            student.Age = 42;
+            //student.DisplayScore();
+            Console.WriteLine(student.ToString());
+            Console.ReadLine();
         }
     }
 }
--- PoP_Rework/Student.cs
+++ PoP_Rework/Student.cs
@@ -8,6 +8,43 @@ namespace PoP_Rework
 {
     public class Student:Person
     {
-
+        public string StudentNumber { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; } //can't assign Address.ExactAddress, etc. so just gonna use a different object and set it
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+            set{}
+        }
+        public int[] Scores { get; set; }
+        public double AverageScore { get; set; }
+        public string FullAddress { get; set; }
+        //{
+        //    get
+        //    {
+        //        return Address.ExactAddress + " " + Address.Street + " " + Address.City + " " + Address.Country;
+        //    }
+        //    set { }
+        //}
+        public void DisplayScore()
+        {
+            foreach (int score in Scores)
+            {
+                Console.Write(score + ",");
+            }
+        }
+        public override string ToString()
+        {
+            return    "Student " + FullName + " age is " + Age
+                    + "\nStudent " + FullName + " average score is " + AverageScore
+                    + "\nStudent " + FullName + " full address is " + FullAddress;
+        }
+        public string SetFullAddres(Address newAddress)
+        {
+            return newAddress.ExactAddress + " " + newAddress.Street + " " + newAddress.City + " " + newAddress.Country;
+        }
     }
 }
